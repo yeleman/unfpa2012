579bb032110e205698ee7daa8388c311cb20a585
Fadiga <ibfadiga@gmail.com>
2012-04-05 09:24:55 +0000

    Correction du sms de product

--- unfpa_sms/products.py
+++ unfpa_sms/products.py
@@ -33,6 +33,7 @@ def unfpa_monthly_product_stockouts(message, args, sub_cmd, **kwargs):
             or [ERREUR] message """
 
     try:
+        args = args.replace("-", "-1")
         reporting_year, reporting_month, location_of_sdp, family_planning, \
         delivery_services, male_condom, female_condom,\
         oral_pills, injectable, iud, implants, female_sterilization, \
@@ -42,7 +43,6 @@ def unfpa_monthly_product_stockouts(message, args, sub_cmd, **kwargs):
         ergometrine_vials, iron, folate, iron_folate, magnesium_sulfate, \
         metronidazole, oxytocine = args.split()
     except:
-        raise
         return resp_error(message, u"le rapport")
     try:
         period = MonthPeriod.find_create_from(year=int(reporting_year), month=int(reporting_month))
@@ -72,30 +72,30 @@ def unfpa_monthly_product_stockouts(message, args, sub_cmd, **kwargs):
     report.created_by = contact_for(message.identity)
     report.family_planning = int(family_planning)
     report.delivery_services = int(delivery_services)
-    report.male_condom = male_condom
-    report.female_condom = female_condom
-    report.oral_pills = oral_pills
-    report.injectable = injectable
-    report.iud = iud
-    report.implants = implants
+    report.male_condom = int(male_condom)
+    report.female_condom = int(female_condom)
+    report.oral_pills = int(oral_pills)
+    report.injectable = int(injectable)
+    report.iud = int(iud)
+    report.implants = int(implants)
     report.female_sterilization = steril_test(female_sterilization)
     report.male_sterilization = steril_test(male_sterilization)
-    report.amoxicillin_ij = amoxicillin_ij
-    report.amoxicillin_cap_gel = amoxicillin_cap_gel
-    report.amoxicillin_suspension = amoxicillin_suspension
-    report.azithromycine_tab = azithromycine_tab
-    report.azithromycine_suspension = azithromycine_suspension
-    report.benzathine_penicillin = benzathine_penicillin
-    report.cefexime = cefexime
-    report.clotrimazole = clotrimazole
-    report.ergometrine_tab = ergometrine_tab
-    report.ergometrine_vials = ergometrine_vials
-    report.iron = iron
-    report.folate = folate
-    report.iron_folate = iron_folate
-    report.magnesium_sulfate = magnesium_sulfate
-    report.metronidazole = metronidazole
-    report.oxytocine = oxytocine
+    report.amoxicillin_ij = int(amoxicillin_ij)
+    report.amoxicillin_cap_gel = int(amoxicillin_cap_gel)
+    report.amoxicillin_suspension = int(amoxicillin_suspension)
+    report.azithromycine_tab = int(azithromycine_tab)
+    report.azithromycine_suspension = int(azithromycine_suspension)
+    report.benzathine_penicillin = int(benzathine_penicillin)
+    report.cefexime = int(cefexime)
+    report.clotrimazole = int(clotrimazole)
+    report.ergometrine_tab = int(ergometrine_tab)
+    report.ergometrine_vials = int(ergometrine_vials)
+    report.iron = int(iron)
+    report.folate = int(folate)
+    report.iron_folate = int(iron_folate)
+    report.magnesium_sulfate = int(magnesium_sulfate)
+    report.metronidazole = int(metronidazole)
+    report.oxytocine = int(oxytocine)
     report.save()
 
     message.respond(u"[SUCCES] Le rapport de %(cscom)s pour %(period)s "
