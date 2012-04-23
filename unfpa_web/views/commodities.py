8c23c85aafb667fa8c5b99509a692c5a3f351dc4
Fadiga <ibfadiga@gmail.com>
2012-04-23 09:29:25 +0000

    Ajout de monthly_commodities

--- unfpa_web/views/commodities.py
+++ unfpa_web/views/commodities.py
@@ -10,6 +10,27 @@ from bolibana.web.decorators import provider_required
 from unfpa_core.models import RHCommoditiesReport
 
 
+
+def check_planing_method(report):
+    i = 0
+    if report.male_condom != -1:
+        i += 1
+    if report.female_condom != -1:
+        i += 1
+    if report.oral_pills != -1:
+        i += 1
+    if report.injectable != -1:
+        i += 1
+    if report.iud != -1:
+        i += 1
+    if report.implants != -1:
+        i += 1
+    if report.female_sterilization == 'T':
+        i += 1
+    if report.male_sterilization == 'T':
+        i += 1
+    return i
+
 @provider_required
 def monthly_commodities(request, period):
 
@@ -27,12 +48,44 @@ def monthly_commodities(request, period):
 
     val = -1
     query = (Q(male_condom=val) | Q(female_condom=val) | Q(oral_pills=val) | Q(injectable=val) |
-             Q(iud=val) | Q(implants=val) | Q(female_sterilization=val) | Q(male_sterilization=val))
+             Q(iud=val) | Q(implants=val) | Q(female_sterilization='N') | Q(male_sterilization='N'))
 
     stoct_out_methods = RHCommoditiesReport.objects.filter(query, period=period)
-    print stoct_out_methods
-
     data.update({"stoct_out_methods": stoct_out_methods})
+
+    all_reports = RHCommoditiesReport.objects.filter(period=period)
+    last_tree_fp = 0
+    for report in all_reports:
+        if check_planing_method(report) >= 3:
+            last_tree_fp += 1
+
+    percent_last_tree_fp = (last_tree_fp * 100) / len(all_reports)
+
+    context.update({"last_tree_fp": last_tree_fp, 
+                 "percent_last_tree_fp": percent_last_tree_fp})
+
+    list_family_planning = RHCommoditiesReport.objects.filter(period=period, family_planning=True)
+    data.update({"list_family_planning": list_family_planning})
+
+    out_Oxytocin_Magnesium_Sulphate = RHCommoditiesReport.objects.filter((Q(oxytocine=val) | Q(magnesium_sulfate=val)), period=period)
+    data.update({"out_Oxytocin_Magnesium_Sulphate": out_Oxytocin_Magnesium_Sulphate})
+    
+    dic = {}
+    for entity in Entity.objects.all():
+        reports_children = []  
+        for child in entity.children.all():
+            
+            try:
+                report = RHCommoditiesReport.objects.get(period=period, entity=child)
+                print report
+                reports_children.append(report)
+            except:
+                report = None
+        print reports_children
+        if reports_children:
+            dic['%s' % entity]= reports_children
+
+    context.update({"dic": dic})  
 
     context.update({'data': data})
 
