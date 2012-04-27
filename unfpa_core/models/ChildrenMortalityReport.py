7cab035de962ae5a66125aaaad2c9760e669bffd
Fadiga <ibfadiga@gmail.com>
2012-04-27 09:38:28 +0000

    Changement du model ChildrenMortalityReport et MaternalMortalityReport

--- unfpa_core/models/ChildrenMortalityReport.py
+++ unfpa_core/models/ChildrenMortalityReport.py
@@ -15,6 +15,17 @@ class ChildrenMortalityReport(IndividualReport):
     NO = 'N'
     YESNO = ((YES, _(u"Yes")), (NO, _(u"No")))
 
+    HOME = "D"
+    CENTER = "C"
+    OTHER = "A"
+    PLACEDEATH = ((HOME, _(u"Domicile")),
+                  (CENTER, _(u"Centre")),
+                  (OTHER, _(u"Autre")))
+
+    MAL = 'M'
+    FEMAL = 'F'
+    SEX = ((FEMAL, _(u"F")), (MAL, _(u"M")))
+
     class Meta:
         app_label = 'unfpa_core'
         verbose_name = _(u"Children Mortality Report")
@@ -22,13 +33,19 @@ class ChildrenMortalityReport(IndividualReport):
 
     reporting_location = models.ForeignKey(Entity,
                                            related_name='children_reported_in',
-                                          verbose_name=_(u"Reporting location"))
+                                           verbose_name=_(u"Reporting location"))
     name = models.CharField(max_length=100,
                             verbose_name=_(u"Name of the deceased"))
+    sex = models.CharField(max_length=1,
+                                   choices=SEX,
+                                   verbose_name=_(u"Sex"))
     dob = models.DateField(verbose_name=_(u"Date of birth"))
     dob_auto = models.BooleanField(default=False,
                                    verbose_name=_(u"DOB is an estimation?"))
     dod = models.DateField(verbose_name=_(u"Date of death"))
+    place_death = models.CharField(max_length=1,
+                                   choices=PLACEDEATH,
+                                   verbose_name=_(u"Place of death"))
     death_location = models.ForeignKey(Entity, related_name='children_dead_in',
                                        verbose_name=_(u"Place of death"))
 
