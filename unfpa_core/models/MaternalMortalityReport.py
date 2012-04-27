0292beff75a120086c7aef7e62afc1cf75f78419
Fadiga <ibfadiga@gmail.com>
2012-04-27 10:41:13 +0000

    Correction du model maternalmortalityreport

--- unfpa_core/models/MaternalMortalityReport.py
+++ unfpa_core/models/MaternalMortalityReport.py
@@ -15,16 +15,6 @@ class MaternalMortalityReport(IndividualReport):
     NO = 'N'
     YESNO = ((YES, _(u"Yes")), (NO, _(u"No")))
 
-    HOME = "D"
-    CENTER = "C"
-    OTHER = "A"
-    PLACEDEATH = ((HOME, _(u"Domicile")),
-                  (CENTER, _(u"Centre")),
-                  (OTHER, _(u"Autre")))
-    MAL = 'M'
-    FEMAL = 'F'
-    SEX = ((FEMAL, _(u"F")), (MAL, _(u"M")))
-
     class Meta:
         app_label = 'unfpa_core'
         verbose_name = _(u"Maternal Mortality Report")
@@ -35,17 +25,10 @@ class MaternalMortalityReport(IndividualReport):
                                           verbose_name=_(u"Reporting location"))
     name = models.CharField(max_length=100,
                             verbose_name=_(u"Name of the deceased"))
-    sex = models.CharField(max_length=1,
-                                   choices=SEX,
-                                   verbose_name=_(u"Sex"))
     dob = models.DateField(verbose_name=_(u"Date of birth"))
     dob_auto = models.BooleanField(default=False,
                                    verbose_name=_(u"DOB is an estimation?"))
     dod = models.DateField(verbose_name=_(u"Date of death"))
-
-    place_death = models.CharField(max_length=1,
-                                   choices=PLACEDEATH,
-                                   verbose_name=_(u"Place of death"))
     death_location = models.ForeignKey(Entity,
                                        related_name='maternal_dead_in',
                                        verbose_name=_(u"Place of death"))
