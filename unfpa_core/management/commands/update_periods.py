3d51ef6a304ca3333994b4ef47d888663fd3c9ec
renaud gaudin <rgaudin@gmail.com>
2012-06-15 13:52:45 +0000

    added quarter commod + period command

---
+++ unfpa_core/management/commands/update_periods.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import ContentType
+
+from datetime import date
+
+from bolibana.models import MonthPeriod, YearPeriod
+
+from unfpa_core.models import (MaternalMortalityReport,
+                               ChildrenMortalityReport,
+                               PregnancyReport,
+                               BirthReport,
+                               RHCommoditiesReport)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        current_period = MonthPeriod.find_create_by_date(date.today())
+
+        # find our date of first report (begining of activities)
+        first_report = date.today()
+        try:
+            child = ChildrenMortalityReport.objects.all().order_by('dod')[0].dod
+        except:
+            child = None
+        try:
+            mat = MaternalMortalityReport.objects.all().order_by('dod')[0].dod
+        except:
+            mat = None
+        try: 
+            preg = PregnancyReport.objects.all().order_by('creatd_on')[0].creatd_on
+        except:
+            preg = None
+        try: 
+            bir = BirthReport.objects.all().order_by('creatd_on')[0].creatd_on
+        except:
+            bir = None
+        try: 
+            commod = RHCommoditiesReport.objects.all().order_by('period')[0].period.start_on
+        except:
+            commod = None
+        if child:
+            first_report = child
+        if mat and mat < first_report:
+            first_report = mat
+        if preg and preg < first_report:
+            first_report = preg
+        if bir and bir < first_report:
+            first_report = bir
+        if commod and commod < first_report:
+            first_report = commod
+
+        first_period = MonthPeriod.find_create_by_date(first_report)
+
+        for year in range(first_period.start_on.year, current_period.end_on.year + 1):
+
+            # create year
+            y = YearPeriod.find_create_from(year)
+            print(y)
+
+            # create quarter
+            for quarter in y.quarters_:
+                print(u"\t%s" % quarter)
+                quarter.save()
+                print(u"\t%s" % quarter)
+
+            # create months:
+            for month in y.months:
+                month.save()
+                print(u"\t\t%s" % month)
+
+                # create weeks
+                for week in month.weeks:
+                    week.save()
+                    print(u"\t\t\t%s" % week)
--- unfpa_web/views/commodities.py
+++ unfpa_web/views/commodities.py
@@ -2,9 +2,11 @@
 # encoding=utf-8
 # maintainer: rgaudin
 
+from datetime import datetime
+
 from django.shortcuts import render
 
-from bolibana.models import Entity, MonthPeriod, QuarterPeriod, YearPeriod
+from bolibana.models import MonthPeriod, QuarterPeriod, YearPeriod
 from bolibana.web.decorators import provider_required
 
 from unfpa_core import unfpa_districts
@@ -106,31 +108,122 @@ def monthly_commodities(request, period):
     return render(request, 'monthly_commodities.html', context)
 
 
-@provider_required
-def quarterly_commodities(request, period):
-
+def quarterly_annual_commodities(request, period, rtype):
     context = {'period': period, 'category': 'unfpa',
                'subcategory': 'commodities'}
-    data = []
-    for district in Entity.objects.filter(type__slug='district'):
-        nb_deaths = 0
-        data.append({'district': district, 'deaths': nb_deaths})
 
-    context.update({'data': data})
+    rtypes = {MonthPeriod: 'monthly',
+              QuarterPeriod: 'quarterly',
+              YearPeriod: 'annual'}
 
-    return render(request, 'quarterly_commodities.html', context)
+    our_periods = [month for month in period.months if month.end_on < datetime.now()]
+    
+    fp_services = [RHCommoditiesReport.validated \
+                                      .filter(period=month) \
+                                      .filter(family_planning=True).count() 
+                   for month in our_periods]
+
+    delivery_services = [RHCommoditiesReport.validated \
+                                            .filter(period=month) \
+                                            .filter(delivery_services=True).count()
+                         for month in our_periods]
+
+    both_services = [RHCommoditiesReport.validated \
+                                     .filter(period=month) \
+                                     .filter(delivery_services=True) \
+                                     .filter(family_planning=True).count()
+                     for month in our_periods]
+
+    fp_stockout = [RHCommoditiesReport.objects.validated() \
+                                      .has_stockouts() \
+                                      .filter(period=month).count()
+                   for month in our_periods]
+
+    atleast_3methods_t = []
+    for month in our_periods:
+        atleast_3methods = sum([1 
+                            for report 
+                            in RHCommoditiesReport.validated.filter(period=period)
+                            if report.fp_stockout_3methods()])
+        try:
+            atleast_3methods_percent = float(atleast_3methods) / \
+                                   RHCommoditiesReport.validated \
+                                                      .filter(period=period).count()
+        except ZeroDivisionError:
+            atleast_3methods_percent = 0
+        atleast_3methods_t.append((atleast_3methods, atleast_3methods_percent))
+
+    otoxycin_magnesium_stockout = [RHCommoditiesReport.validated \
+                                     .filter(period=month) \
+                                     .filter(magnesium_sulfate=0) \
+                                     .filter(oxytocine=0).count()
+                                   for month in our_periods]
+
+    context.update({'fp_services': fp_services,
+                    'delivery_services': delivery_services,
+                    'both_services': both_services,
+                    'fp_stockout': fp_stockout,
+                    'atleast_3methods': atleast_3methods_t,
+                    'otoxycin_magnesium_stockout':otoxycin_magnesium_stockout})
+
+
+    # districts
+    all_stock_outs = []
+    for district in unfpa_districts():
+        centers = district.children \
+                          .filter(type__slug__in=('cscom', 
+                                                  'csref', 'hospital'))
+        nb_centers = float(centers.count())
+
+        methods = ('male_condom', 'female_condom', 'oral_pills',
+                           'injectable', 'iud', 'implants', 
+                           'female_sterilization', 'male_sterilization',
+                           'magnesium_sulfate', 'oxytocine')
+
+        month_stock_outs = []
+        for month in our_periods:
+
+            stock_outs = {}
+            for method in methods:
+                stock_outs[method] = [0, 0]
+
+            reports = RHCommoditiesReport.objects.validated().filter(period=month, 
+                                                                 entity__in=centers)
+            for report in reports:
+                for method in methods:
+                    # increment counter of # of center with stockout.
+                    if getattr(report, method, report.SUPPLIES_NOT_PROVIDED) == 0:
+                        stock_outs[method][0] += 1
+
+            # compute percentages
+            for key, method_so in stock_outs.items():
+                try:
+                    stock_outs[key] = (method_so[0], method_so[0] / nb_centers)
+                except ZeroDivisionError:
+                    pass
+
+            stock_outs.update({'month': month})
+            month_stock_outs.append(stock_outs)
+
+        all_stock_outs.append({'district': district,
+                               'stock_outs': month_stock_outs,
+                               'nb_centers': int(nb_centers),
+                               'reports': reports})
+
+    context.update({'all_stock_outs': all_stock_outs,})
+
+    context.update({'type': rtypes.get(period.__class__, MonthPeriod),
+                    'our_periods': our_periods})
+
+    return render(request, 'quarterly_annual_commodities.html', context)
+
+
+
+@provider_required
+def quarterly_commodities(request, period):
+    return quarterly_annual_commodities(request, period, 'quarterly')
 
 
 @provider_required
 def annual_commodities(request, period):
-
-    context = {'period': period, 'category': 'unfpa',
-               'subcategory': 'commodities'}
-    data = []
-    for district in Entity.objects.filter(type__slug='district'):
-        nb_deaths = 0
-        data.append({'district': district, 'deaths': nb_deaths})
-
-    context.update({'data': data})
-
-    return render(request, 'annual_commodities.html', context)
+    return quarterly_annual_commodities(request, period, 'annual')
