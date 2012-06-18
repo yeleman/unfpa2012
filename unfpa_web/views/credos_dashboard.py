54465163cf84a94c4389ffdfed7f8036df14397e
Alou Dolo <alou@yeleman.com>
2012-06-18 16:54:02 +0000

    push non propre

--- unfpa_web/views/credos_dashboard.py
+++ unfpa_web/views/credos_dashboard.py
@@ -5,10 +5,33 @@
 from django.shortcuts import render
 
 from bolibana.web.decorators import provider_required
+from unfpa_core.models import ChildrenMortalityReport, PregnancyReport
+
+from unfpa_web.views.data import current_period
 
 
 @provider_required
 def credos_dashboard(request):
     context = {'category': 'credos','subcategory': 'credos_dashboard'}
+
+    period = current_period()
+    total_children = ChildrenMortalityReport.objects.all().count()
+    last_total_children = ChildrenMortalityReport.objects \
+                                .filter(created_on__gte=period.start_on,
+                                        created_on__lte=period.end_on) \
+                                .count()    
+    total_pregnancy = PregnancyReport.objects.all().count()
+    last_total_pregnancy = PregnancyReport.objects \
+                                .filter(created_on__gte=period.start_on,
+                                        created_on__lte=period.end_on) \
+                                .count()
+    print last_total_pregnancy, 'alou'
+    context.update({'period': period, 
+                    'total_children': total_children,
+                    'last_total_children': last_total_children,
+                    'total_pregnancy': total_pregnancy,
+                    'last_total_pregnancy': last_total_pregnancy})
+    print total_children, last_total_children
+
 
     return render(request, 'credos_dashboard.html', context)
